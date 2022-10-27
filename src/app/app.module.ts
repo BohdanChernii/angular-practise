ea25322be24e993a93374f94fd3ab5b5f5072059
promius <bodiachernii@gmail.com>
2022-10-27 11:59:34 +0300

    lesson1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import {AppComponent} from './app.component';
 import {CommonModule} from "@angular/common";
 import {HttpClientModule} from "@angular/common/http";
 import {BrowserModule} from "@angular/platform-browser";
+import { CommentsComponent } from './components/comments/comments.component';
+import { UsersComponent } from './components/users/users.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { UserComponent } from './components/user/user.component';
+import { PostComponent } from './components/post/post.component';
+import { CommentComponent } from './components/comment/comment.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    CommentsComponent,
+    UsersComponent,
+    PostsComponent,
+    UserComponent,
+    PostComponent,
+    CommentComponent,
 
   ],
   imports: [
---
+++ src/app/interfaces/comment.interface.ts
@@ -0,0 +1,9 @@
+interface IComment {
+  postId: number,
+  id: number,
+  name: string,
+  body: string,
+  email: string,
+}
+
+export type {IComment}
